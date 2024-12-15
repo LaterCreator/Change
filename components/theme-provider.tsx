5074c55b83c51eb6ec2ac5b585fb060d2f2e7600
charles <dol321holli@gmail.com>
2024-12-15 11:38:14 +0100

    sds

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,8 @@
-"use client"
-
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+// components/theme-provider.tsx
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes"; // Correct import
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-
